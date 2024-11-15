77192094f5bcc732a381517842a9804f2a70bad6
Varga Máté <varga.mate.gellert@students.jedlik.eu>
2024-11-15 11:21:23 +0100

    first commit

---
+++ routes/routes.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const nSideModel = require("../models/model_n");
+const oneSideModel = require("../models/model_1");
+
+const router = express.Router();
+
+router.post("/", async (req, res) => {
+  const data = new nSideModel({
+    name: req.body.name,
+    foreignKey: req.body.foreignKey,
+  });
+
+  try {
+    
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+router.get("/", async (req, res) => {
+  try {
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+//Update by ID Method
+router.patch("/:id", async (req, res) => {
+  try {
+
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+//Delete by ID Method
+router.delete("/:id", async (req, res) => {
+  try {
+
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+module.exports = router;
